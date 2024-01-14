e4fbc35229c04275b18aff9dac954fe574481517
cyberspacex <126796642+oluwatay123@users.noreply.github.com>
2024-01-14 22:32:54 +0100

    Add files via upload

--- calc.js
+++ calc.js
@@ -6,91 +6,77 @@ let buttons = document.querySelectorAll("button");
 let btnArr = Array.from(buttons);
 
 let eqnBtn = document.querySelector(".equalBtn");
-const oprBtn =document.querySelectorAll('.operator')
+const oprBtn = document.querySelectorAll(".operator");
 
 let string = "";
 
-const btnDark = document.querySelector('.dark_mode')
+const btnDark = document.querySelector(".dark_mode");
 
-const container = document.querySelector('.calculator')
+const container = document.querySelector(".calculator");
 
-const slider = document.querySelector('.slider.round')
+const sliderContainer = document.querySelector(".slider_container");
 
-// const inputChecked = document.querySelector('input')
+const slider = document.querySelector(".slide");
 
-//update screen 
-const updateScreen = function(){
-    screen.value = string
-}
+//update screen
+const updateScreen = function () {
+  screen.value = string;
+};
 
-//check for invalid opeartion 
-// const checkInavlidOpreation =function(action){
-//   if   (/[\+\-\*\/]{2,}/.test(action) || /[\+\-\*\/]$/.test(action)){
-    // screen.value = `Error`
-//   }
-// 
-// }
-// creating the calculation logic
 const calcLogic = (action) => {
-//   const action = e.target.innerHTML;
+  //   const action = e.target.innerHTML;
 
   if (action === "=") {
     equalToCheck();
   } else if (action === "AC") {
     string = "";
-    
   } else if (action === "DEL") {
     string = string.substring(0, string.length - 1);
-  
   } else {
-    string += action; 
+    string += action;
   }
- 
-  
-  updateScreen()
+
+  updateScreen();
 };
 // set some conditions for equalto operator
 const equalToCheck = function () {
   string = eval(string);
-  
-try{
-  if (isNaN(string) || !isFinite(string)) {
-    throw new Error("Invalid input");
+
+  try {
+    if (isNaN(string) || !isFinite(string)) {
+      throw new Error("Invalid input");
+    }
+  } catch (error) {
+    string = "";
   }
-  
-} catch (error) {
-  string = "";
-}
-updateScreen()
+  updateScreen();
 };
 
 // event to all buttons
 btnArr.forEach((button) => {
-  button.addEventListener("click",()=>{
-    const action = button.textContent
-   
-    calcLogic(action)
+  button.addEventListener("click", () => {
+    const action = button.textContent;
+
+    calcLogic(action);
   });
 });
 
-//create darkmode 
-btnDark.addEventListener('click', ()=>{
-    document.body.classList.toggle('darkMode')
-//    document.querySelector('button').classList.toggle('darkMode')
-buttons.forEach(element=>{
-    element.classList.toggle('darkBtn')
-})
-eqnBtn.classList.toggle('darkEqn')
-oprBtn.forEach(element=>{
-    element.classList.toggle('darkOpsBtn')
-})
-screen.classList.toggle('darkInput')
+// for enabling dark mode
 
-container.classList.toggle('darkContainer')
+sliderContainer.addEventListener("click", () => {
+  slider.classList.toggle("movement");
 
-// inputChecked.classList.toggle('movement')
+  document.body.classList.toggle("darkMode");
+  buttons.forEach((element) => {
+    element.classList.toggle("darkBtn");
+  });
+  eqnBtn.classList.toggle("darkEqn");
+  oprBtn.forEach((element) => {
+    element.classList.toggle("darkOpsBtn");
+  });
+  screen.classList.toggle("darkInput");
 
-slider.classList.toggle('movement')
+  container.classList.toggle("darkContainer");
 
-})
-
+  sliderContainer.classList.toggle("slider_color_change");
+});
